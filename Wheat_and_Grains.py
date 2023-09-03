710266199682f0c4f268761a7ce27061c5053918
Jess Gallo <54815820+Gallo13@users.noreply.github.com>
2023-09-03 14:10:41 -0400

    Create Wheat_and_Grains.py

---
+++ Wheat_and_Grains.py
@@ -0,0 +1,20 @@
+"""
+Calculate the number of grains of wheat on a chessboard given that the number on each square doubles.
+
+There are 64 squares on a chessboard (where square 1 has one grain, square 2 has two grains, and so on).
+
+Write code that shows:
+
+1) how many grains were on a given square, and
+2) the total number of grains on the chessboard
+"""
+
+def square(number):
+    if number >= 1 and number <= 64:
+        return 1 << (number - 1)
+    else:
+        raise ValueError('square must be between 1 and 64')
+
+
+def total():
+    return (1 << 64) - 1
