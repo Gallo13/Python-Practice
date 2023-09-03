75dadbaa4e16c8bad0436c76d01ae4ebfcf1fad4
Jess Gallo <54815820+Gallo13@users.noreply.github.com>
2023-09-03 14:23:54 -0400

    Added another way to solve

--- Wheat_and_Grains.py
+++ Wheat_and_Grains.py
@@ -18,3 +18,13 @@ def square(number):
 
 def total():
     return (1 << 64) - 1
+
+##############################################################
+
+def square(number):    
+    if number <= 0 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    return 2**(number-1)
+    
+def total(number=64):
+    return sum(2**(item) for item in range(number))
